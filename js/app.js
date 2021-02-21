ed355e0a1aec7d0b028c39f6f27025a1dafdba34
MohdAzzam <alazzamhmode@gmail.com>
2021-02-21 17:06:10 +0200

    add .eslintrc and .gitignore

--- js/app.js
+++ js/app.js
@@ -7,16 +7,16 @@ let allObject = [];
 let container = document.getElementById('container');
 
 function BussMall(productName, imgSource) {
-    this.productName = productName;
-    this.imgSource = 'img/' + imgSource;
-    this.imgeCount = 0;
-    this.imgShown = 0;
-    allObject.push(this);
+  this.productName = productName;
+  this.imgSource = 'img/' + imgSource;
+  this.imgeCount = 0;
+  this.imgShown = 0;
+  allObject.push(this);
 }
 
 new BussMall('bag', 'bag.jpg');
 new BussMall('banana', 'banana.jpg');
-new BussMall('bathroom', 'bathroom.jpg')
+new BussMall('bathroom', 'bathroom.jpg');
 new BussMall('boots', 'boots.jpg');
 new BussMall('breakfast', 'breakfast.jpg');
 new BussMall('bubblegum', 'bubblegum.jpg');
@@ -39,82 +39,75 @@ let imgOneIndex;
 let imgTwoIndex;
 let imgThreeIndex;
 function renderThreeRandomImg() {
-    imgOneIndex = generateRandomImg();
+  imgOneIndex = generateRandomImg();
+  imgTwoIndex = generateRandomImg();
+  imgThreeIndex = generateRandomImg();
+
+  while ((imgOneIndex === imgTwoIndex) || (imgOneIndex === imgThreeIndex) || (imgTwoIndex === imgThreeIndex)) {
     imgTwoIndex = generateRandomImg();
     imgThreeIndex = generateRandomImg();
-    // console.log(`${imgOneIndex}  ${imgTwoIndex} ${imgThreeIndex}`);
-    // console.log('--------------------------------------');
 
-    while ((imgOneIndex === imgTwoIndex) || (imgOneIndex === imgThreeIndex) || (imgTwoIndex === imgThreeIndex)) {
-        imgTwoIndex = generateRandomImg();
-        imgThreeIndex = generateRandomImg();
+  }
 
-    }
-
-    console.log(`${imgOneIndex}  ${imgTwoIndex} ${imgThreeIndex}`);
-    let imgOne = document.getElementById('imgOne');
-    let imgTwo = document.getElementById('imgTwo');
-    let imgThree = document.getElementById('imgThree');
-    imgOne.setAttribute('src', allObject[imgOneIndex].imgSource);
-    imgTwo.setAttribute('src', allObject[imgTwoIndex].imgSource);
-    imgThree.setAttribute('src', allObject[imgThreeIndex].imgSource);
-    allObject[imgOneIndex].imgeCount++;
-    allObject[imgTwoIndex].imgeCount++;
-    allObject[imgThreeIndex].imgeCount++;
-    //    console.log(`${ allObject[imgOneIndex].imgeCount} ${ allObject[imgTwoIndex].imgeCount} ${ allObject[imgThreeIndex].imgeCount}`);
+  console.log(`${imgOneIndex}  ${imgTwoIndex} ${imgThreeIndex}`);
+  let imgOne = document.getElementById('imgOne');
+  let imgTwo = document.getElementById('imgTwo');
+  let imgThree = document.getElementById('imgThree');
+  imgOne.setAttribute('src', allObject[imgOneIndex].imgSource);
+  imgTwo.setAttribute('src', allObject[imgTwoIndex].imgSource);
+  imgThree.setAttribute('src', allObject[imgThreeIndex].imgSource);
+  allObject[imgOneIndex].imgeCount++;
+  allObject[imgTwoIndex].imgeCount++;
+  allObject[imgThreeIndex].imgeCount++;
 
 }
 
 
 
 function generateRandomImg() {
-    let random = Math.floor(Math.random() * allObject.length);
-    return random;
+  let random = Math.floor(Math.random() * allObject.length);
+  return random;
 }
 
 renderThreeRandomImg();
 
 container.addEventListener('click', imgClik);
-// console.log(allObject[0].imgeCount);
 
 function imgClik(event) {
-    console.log(event);
+  console.log(event);
 
-    if (userTry <= maxClicks) {
-        
-        console.log(userTry);
-        if (event.target.id === 'imgOne') {
-            userTry++;
-            allObject[imgOneIndex].imgShown++;
-        }
-        if (event.target.id === 'imgTwo') {
-            userTry++;
-            allObject[imgTwoIndex].imgShown++;
-        }
-        if (event.target.id === 'imgThree') {
-            userTry++;
-            allObject[imgThreeIndex].imgShown++;
-        }
-        event.preventDefault();
-        renderThreeRandomImg();
-    } else {
-        let list = document.getElementById('list');
-        let li;
-        for (let j = 0; j < allObject.length; j++) {
-            li = document.createElement('li');
-            list.appendChild(li);
-            li.textContent = `${allObject[j].productName} it has Chossed ${allObject[j].imgShown} and it Show ${allObject[j].imgeCount}`;
-        }
-        container.removeEventListener('click', imgClik);
-        document.getElementById('btn').style.display='block';
+  if (userTry <= maxClicks) {
+
+    console.log(userTry);
+    if (event.target.id === 'imgOne') {
+      userTry++;
+      allObject[imgOneIndex].imgShown++;
     }
+    if (event.target.id === 'imgTwo') {
+      userTry++;
+      allObject[imgTwoIndex].imgShown++;
+    }
+    if (event.target.id === 'imgThree') {
+      userTry++;
+      allObject[imgThreeIndex].imgShown++;
+    }
+    event.preventDefault();
+    renderThreeRandomImg();
+  } else {
+    let list = document.getElementById('list');
+    let li;
+    for (let j = 0; j < allObject.length; j++) {
+      li = document.createElement('li');
+      list.appendChild(li);
+      li.textContent = `${allObject[j].productName} it has Chossed ${allObject[j].imgShown} and it Show ${allObject[j].imgeCount}`;
+    }
+    container.removeEventListener('click', imgClik);
+    document.getElementById('btn').style.display = 'block';
+  }
 
 
 }
 
-function showul(){
-    document.getElementById('list').style.display='block';
-    // document.getElementById('list').style.display='none';
-
-
+function showul() {
+  document.getElementById('list').style.display = 'block';
 }
